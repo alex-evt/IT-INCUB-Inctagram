df35450aebcf242639a8cd52fb6463e51df980a6
Alex <Alexander.evtukh@yandex.ru>
2023-11-03 23:10:33 +0300

    Framework initialization

---
+++ src/main/java/driver/DriverSingleton.java
@@ -0,0 +1,40 @@
+package driver;
+
+import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.WebDriver;
+
+@Log4j2
+public class DriverSingleton {
+
+    private final static ThreadLocal<DriverSingleton> instance = new ThreadLocal<>();
+    private WebDriver driver;
+
+    private DriverSingleton(){
+        driver = WebDriverFactory.getWebDriver();
+    }
+
+    public static synchronized DriverSingleton getInstance(){
+        if(instance.get() == null){
+            instance.set(new DriverSingleton());
+            log.info("Instance of the driver was created");
+        }
+        return instance.get();
+    }
+
+    public WebDriver getDriver(){
+        return driver;
+    }
+
+    public void closeDriver(){
+        try{
+            driver.quit();
+            driver = null;
+            log.info("Driver closed successfully");
+        }catch (Exception e){
+            log.fatal("Driver wasn't closed" + e.getMessage());
+        }
+        finally {
+            instance.remove();
+        }
+    }
+}
---
+++ src/main/java/driver/WebDriverFactory.java
@@ -0,0 +1,49 @@
+package driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.core.config.plugins.PluginLoggerContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
+
+@Log4j2
+public class WebDriverFactory {
+
+    private WebDriverFactory() {
+    }
+
+    public static WebDriver getWebDriver() {
+        WebDriver driver = null;
+        try {
+            switch (System.getProperty("browser")) {
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    break;
+                case "opera":
+                    WebDriverManager.operadriver().setup();
+                    driver = new ChromeDriver(); //???
+                    break;
+                default:                   //Chrome is default
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--incognito");
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(chromeOptions);
+            }
+            log.info("WebDriver initialized");
+        } catch (Exception e) {
+            log.fatal("Failed to initialize WebDriver" + e.getMessage());
+        }
+        return driver;
+    }
+
+
+}
